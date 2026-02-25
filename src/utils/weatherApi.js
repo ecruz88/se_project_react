7ddb32d661817f8a677a3afd5c6bdb0e8fca81a4
estefania.cruz <ecruz8805@gmail.com>
2026-02-24 21:24:03 -0500

    changes made

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,8 +1,8 @@
- import { APIkey } from "./constants";
+import { APIkey } from "./constants";
 
 export const getWeather = ({ latitude, longitude }) => {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`,
   ).then((res) => {
     if (res.ok) {
       return res.json();
@@ -11,29 +11,27 @@ export const getWeather = ({ latitude, longitude }) => {
   });
 };
 
+export const filterWeatherData = (data) => {
+  const result = {};
+  result.city = data.name;
+  result.temp = { F: data.main.temp };
+  result.type = getWeatherType(result.temp.F);
+  result.condition = data.weather[0].main.toLowerCase();
+  result.isDay = isDay(data.sys, Date.now());
 
-  export const filterWeatherData = (data) => {
-    const result = {};
-    result.city = data.name;
-    result.temp = { F: data.main.temp };
-    result.type = getWeatherType(result.temp.F);
-    result.condition = data.weather[0].main.toLowerCase();
-    result.isDay = isDay(data.sys, Date.now())
+  return result;
+};
 
-    return result;
- }
+const isDay = ({ sunrise, sunset }, now) => {
+  return sunrise * 1000 < now && now < sunset * 1000;
+};
 
- const isDay = ({sunrise, sunset}, now) => {
-    return sunrise * 1000 < now && now < sunset * 1000;
- };
-
- const getWeatherType = (temperature) => {
-   if (temperature >= 86) {
-   return 'hot';
- } else if (temperature >= 66 && temperature < 86) {
-   return 'warm';
- } else {
-   return 'cold';
- }
- }
-
+const getWeatherType = (temperature) => {
+  if (temperature >= 86) {
+    return "hot";
+  } else if (temperature >= 66 && temperature < 86) {
+    return "warm";
+  } else {
+    return "cold";
+  }
+};
